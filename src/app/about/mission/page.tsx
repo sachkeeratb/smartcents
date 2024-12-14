2f9622ab8d5adb2ea069302db8f6d42f71cf5acb
Sachkeerat Singh Brar <sachkeeratbrar@gmail.com>
2024-12-14 02:07:45 -0500

    Removed unused 'badge'

--- src/app/about/mission/page.tsx
+++ src/app/about/mission/page.tsx
@@ -9,10 +9,6 @@ export default function Mission() {
 			<div className='max-w-full mx-auto antialiased pt-4 relative'>
 				{content.map((item, index) => (
 					<div key={`content-${index}`} className='mb-10'>
-						<h2 className='bg-black text-white rounded-full text-sm w-fit px-4 py-1 mb-4'>
-							{item.badge}
-						</h2>
-
 						<p className={'text-xl mb-4'}>{item.title}</p>
 
 						<div className='text-md  prose prose-sm dark:prose-invert'>
