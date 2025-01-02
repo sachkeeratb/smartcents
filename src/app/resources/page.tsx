7bce6b63c4bebbfa0c6ed2147be795ebaad25d53
Sachkeerat Singh Brar <sachkeeratbrar@gmail.com>
2025-01-01 22:09:01 -0500

    Added resources

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -2,16 +2,17 @@
 import { cn } from '@/lib/utils';
 import React, { useState, useEffect } from 'react';
 import { BentoGrid, BentoGridItem } from '@/components/ui/bento-grid';
-import {
-	IconBoxAlignRightFilled,
-	IconClipboardCopy,
-	IconFileBroken,
-	IconSignature,
-	IconTableColumn
-} from '@tabler/icons-react';
-import { motion } from 'framer-motion';
-import Image from 'next/image';
-import anonymous from '@/components/images/anonymous.png';
+import { isIPad } from '@/hooks/isMobile';
+
+import resource1 from '@/components/images/resource1.png';
+import resource2 from '@/components/images/resource2.png';
+import resource3 from '@/components/images/resource3.png';
+import resource4 from '@/components/images/resource4.png';
+import resource5 from '@/components/images/resource5.png';
+import resource6 from '@/components/images/resource6.png';
+import resource7 from '@/components/images/resource7.png';
+import resource8 from '@/components/images/resource8.png';
+import resource9 from '@/components/images/resource9.png';
 
 export default function Resources() {
 	const [searchTerm, setSearchTerm] = useState('');
@@ -63,7 +64,7 @@ export default function Resources() {
 			<div
 				className={`transition-all duration-250 ${
 					isSearching ? 'blur-sm' : ''
-				}`}
+				} `}
 			>
 				<BentoGrid className='max-w-10xl mx-auto md:auto-rows-[20rem]'>
 					{filteredResources.map((resource, i) => (
@@ -71,9 +72,13 @@ export default function Resources() {
 							key={i}
 							title={resource.title}
 							description={resource.description}
-							header={resource.header}
-							className={cn('[&>p:text-lg]', resource.className)}
-							icon={resource.icon}
+							image={resource.image}
+							link={resource.link}
+							header={<div />}
+							className={cn(
+								'[&>p:text-lg]',
+								`md:col-span-${isIPad() ? '2 ml-[8rem] mr-[-8rem]' : '1'}`
+							)}
 						/>
 					))}
 				</BentoGrid>
@@ -82,326 +87,100 @@ export default function Resources() {
 	);
 }
 
-const SkeletonOne = () => {
-	const variants = {
-		initial: {
-			x: 0
-		},
-		animate: {
-			x: 10,
-			rotate: 5,
-			transition: {
-				duration: 0.2
-			}
-		}
-	};
-	const variantsSecond = {
-		initial: {
-			x: 0
-		},
-		animate: {
-			x: -10,
-			rotate: -5,
-			transition: {
-				duration: 0.2
-			}
-		}
-	};
-
-	return (
-		<motion.div
-			initial='initial'
-			whileHover='animate'
-			className='flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-col space-y-2'
-		>
-			<motion.div
-				variants={variants}
-				className='flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2  items-center space-x-2 bg-white dark:bg-black'
-			>
-				<div className='h-6 w-6 rounded-full bg-gradient-to-r from-pink-500 to-violet-500 flex-shrink-0' />
-				<div className='w-full bg-gray-100 h-4 rounded-full dark:bg-neutral-900' />
-			</motion.div>
-			<motion.div
-				variants={variantsSecond}
-				className='flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2 items-center space-x-2 w-3/4 ml-auto bg-white dark:bg-black'
-			>
-				<div className='w-full bg-gray-100 h-4 rounded-full dark:bg-neutral-900' />
-				<div className='h-6 w-6 rounded-full bg-gradient-to-r from-pink-500 to-violet-500 flex-shrink-0' />
-			</motion.div>
-			<motion.div
-				variants={variants}
-				className='flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2 items-center space-x-2 bg-white dark:bg-black'
-			>
-				<div className='h-6 w-6 rounded-full bg-gradient-to-r from-pink-500 to-violet-500 flex-shrink-0' />
-				<div className='w-full bg-gray-100 h-4 rounded-full dark:bg-neutral-900' />
-			</motion.div>
-		</motion.div>
-	);
-};
-const SkeletonTwo = () => {
-	const variants = {
-		initial: {
-			width: 0
-		},
-		animate: {
-			width: '100%',
-			transition: {
-				duration: 0.2
-			}
-		},
-		hover: {
-			width: ['0%', '100%'],
-			transition: {
-				duration: 2
-			}
-		}
-	};
-	const arr = new Array(6).fill(0);
-	return (
-		<motion.div
-			initial='initial'
-			animate='animate'
-			whileHover='hover'
-			className='flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-col space-y-2'
-		>
-			{arr.map((_, i) => (
-				<motion.div
-					key={'skelenton-two' + i}
-					variants={variants}
-					style={{
-						maxWidth: Math.random() * (100 - 40) + 40 + '%'
-					}}
-					className='flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2  items-center space-x-2 bg-neutral-100 dark:bg-black w-full h-4'
-				></motion.div>
-			))}
-		</motion.div>
-	);
-};
-const SkeletonThree = () => {
-	const variants = {
-		initial: {
-			backgroundPosition: '0 50%'
-		},
-		animate: {
-			backgroundPosition: ['0, 50%', '100% 50%', '0 50%']
-		}
-	};
-	return (
-		<motion.div
-			initial='initial'
-			animate='animate'
-			variants={variants}
-			transition={{
-				duration: 5,
-				repeat: Infinity,
-				repeatType: 'reverse'
-			}}
-			className='flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] rounded-lg bg-dot-black/[0.2] flex-col space-y-2'
-			style={{
-				background:
-					'linear-gradient(-45deg, #ee7752, #e73c7e, #23a6d5, #23d5ab)',
-				backgroundSize: '400% 400%'
-			}}
-		>
-			<motion.div className='h-full w-full rounded-lg'></motion.div>
-		</motion.div>
-	);
-};
-const SkeletonFour = () => {
-	const first = {
-		initial: {
-			x: 20,
-			rotate: -5
-		},
-		hover: {
-			x: 0,
-			rotate: 0
-		}
-	};
-	const second = {
-		initial: {
-			x: -20,
-			rotate: 5
-		},
-		hover: {
-			x: 0,
-			rotate: 0
-		}
-	};
-	return (
-		<motion.div
-			initial='initial'
-			animate='animate'
-			whileHover='hover'
-			className='flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-row space-x-2'
-		>
-			<motion.div
-				variants={first}
-				className='h-full w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center'
-			>
-				<Image
-					src={anonymous}
-					alt='avatar'
-					height='100'
-					width='100'
-					className='rounded-full h-10 w-10'
-				/>
-				<p className='sm:text-sm text-xs text-center font-semibold text-neutral-500 mt-4'>
-					I don&apos;t need financial advice
-				</p>
-				<p className='border border-red-500 bg-red-100 dark:bg-red-900/20 text-red-600 text-xs rounded-full px-2 py-0.5 mt-4'>
-					Delusional
-				</p>
-			</motion.div>
-			<motion.div className='h-full relative z-20 w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center'>
-				<Image
-					src={anonymous}
-					alt='avatar'
-					height='100'
-					width='100'
-					className='rounded-full h-10 w-10'
-				/>
-				<p className='sm:text-sm text-xs text-center font-semibold text-neutral-500 mt-4'>
-					I always attend SmartCents meetings!
-				</p>
-				<p className='border border-green-500 bg-green-100 dark:bg-green-900/20 text-green-600 text-xs rounded-full px-2 py-0.5 mt-4'>
-					Sensible
-				</p>
-			</motion.div>
-			<motion.div
-				variants={second}
-				className='h-full w-1/3 rounded-2xl bg-white p-4 dark:bg-black dark:border-white/[0.1] border border-neutral-200 flex flex-col items-center justify-center'
-			>
-				<Image
-					src={anonymous}
-					alt='avatar'
-					height='100'
-					width='100'
-					className='rounded-full h-10 w-10'
-				/>
-				<p className='sm:text-sm text-xs text-center font-semibold text-neutral-500 mt-4'>
-					I&apos;ll just thug it out.
-				</p>
-				<p className='border border-orange-500 bg-orange-100 dark:bg-orange-900/20 text-orange-600 text-xs rounded-full px-2 py-0.5 mt-4'>
-					Helpless
-				</p>
-			</motion.div>
-		</motion.div>
-	);
-};
-const SkeletonFive = () => {
-	const variants = {
-		initial: {
-			x: 0
-		},
-		animate: {
-			x: 10,
-			rotate: 5,
-			transition: {
-				duration: 0.2
-			}
-		}
-	};
-	const variantsSecond = {
-		initial: {
-			x: 0
-		},
-		animate: {
-			x: -10,
-			rotate: -5,
-			transition: {
-				duration: 0.2
-			}
-		}
-	};
-
-	return (
-		<motion.div
-			initial='initial'
-			whileHover='animate'
-			className='flex flex-1 w-full h-full min-h-[6rem] dark:bg-dot-white/[0.2] bg-dot-black/[0.2] flex-col space-y-2'
-		>
-			<motion.div
-				variants={variants}
-				className='flex flex-row rounded-2xl border border-neutral-100 dark:border-white/[0.2] p-2  items-start space-x-2 bg-white dark:bg-black'
-			>
-				<Image
-					src={anonymous}
-					alt='avatar'
-					height='100'
-					width='100'
-					className='rounded-full h-10 w-10'
-				/>
-				<p className='text-xs text-neutral-500'>
-					There are a lot of cool places out there to learn about finance and
-					apply your knowledge in your life.
-				</p>
-			</motion.div>
-			<motion.div
-				variants={variantsSecond}
-				className='flex flex-row rounded-full border border-neutral-100 dark:border-white/[0.2] p-2 items-center justify-end space-x-2 w-3/4 ml-auto bg-white dark:bg-black'
-			>
-				<p className='text-xs text-neutral-500'>Join SmartCents.</p>
-				<div className='h-6 w-6 rounded-full bg-gradient-to-r from-pink-500 to-violet-500 flex-shrink-0' />
-			</motion.div>
-		</motion.div>
-	);
-};
-
 const resources = [
 	{
-		title: 'Investment Strategies',
+		title: 'Workshop 1',
+		image: resource1.src,
+		link: 'https://www.canva.com/design/DAGV9IO88Nw/SiUYYAoli0sK_Oe6E4Wwpg/view?utlId=h5bb84aa428#1',
 		description: (
 			<span className='text-sm'>
-				Learn about various investment strategies to grow your wealth.
+				Our first workshop, covering SMART Goals, Budgeting, and more.
 			</span>
-		),
-		header: <SkeletonOne />,
-		className: 'md:col-span-1',
-		icon: <IconClipboardCopy className='h-4 w-4 text-neutral-500' />
+		)
 	},
 	{
-		title: 'Budgeting Tips',
+		title: 'Workshop 2',
+		image: resource2.src,
+		link: 'https://www.canva.com/design/DAGZyPb-oiE/o2FypPhQ8C3gbVLu2PrbKg/view?utlId=h255c7fb878#1',
 		description: (
 			<span className='text-sm'>
-				Discover effective budgeting tips to manage your finances better.
+				Our second workshop, covering Saving, Investing, and Credit/Debit.
 			</span>
-		),
-		header: <SkeletonTwo />,
-		className: 'md:col-span-1',
-		icon: <IconFileBroken className='h-4 w-4 text-neutral-500' />
+		)
 	},
 	{
-		title: 'Retirement Planning',
+		title: 'MNP - Debt - Financial Literacy in Canada',
+		image: resource3.src,
+		link: 'https://mnpdebt.ca/en/resources/mnp-debt-blog/financial-literacy-in-canada-lack-of-skills-and-education-contributing-to-surging-consumer-debt',
 		description: (
 			<span className='text-sm'>
-				Get insights on planning for a secure retirement.
+				A lack Of skills and education contributing to surging consumer debt.
 			</span>
-		),
-		header: <SkeletonThree />,
-		className: 'md:col-span-1',
-		icon: <IconSignature className='h-4 w-4 text-neutral-500' />
+		)
 	},
 	{
-		title: 'Tax Optimization',
+		title: 'CIBC - Poll - Most Canadians wish to be more financially literate',
+		image: resource4.src,
+		link: 'https://www.newswire.ca/news-releases/most-canadians-wish-they-were-more-financially-literate-cibc-poll-841005652.html',
 		description: (
 			<span className='text-sm'>
-				Learn how to optimize your taxes and save money.
+				According to a 2023 poll, a majority of Canadians wish they were more
+				financially literate, showing a need for more advanced/specialized help.
 			</span>
-		),
-		header: <SkeletonFour />,
-		className: 'md:col-span-2',
-		icon: <IconTableColumn className='h-4 w-4 text-neutral-500' />
+		)
 	},
 	{
-		title: 'Debt Management',
+		title: 'CISION - Survey - Canadian Youth',
+		image: resource5.src,
+		link: 'https://www.newswire.ca/news-releases/canadian-youth-need-to-better-understand-how-money-works-and-how-to-use-it-wisely-893368603.html',
 		description: (
 			<span className='text-sm'>
-				Find strategies to manage and reduce your debt effectively.
+				Young Canadians (aged 18-34) are among the least likely (40 per cent) to
+				agree they had the required knowledge upon graduation.
 			</span>
-		),
-		header: <SkeletonFive />,
-		className: 'md:col-span-1',
-		icon: <IconBoxAlignRightFilled className='h-4 w-4 text-neutral-500' />
+		)
+	},
+	{
+		title: 'Gov. of Canada - Pedagogical Solution to Financial Illiteracy',
+		image: resource6.src,
+		link: 'https://www.canada.ca/en/financial-consumer-agency/programs/research/2022-building-better-financial-futures-challenge/pedagogical-solution-financial-literacy-canada.html',
+		description: (
+			<span className='text-sm'>
+				Improving Financial Literacy: Access to Higher Education.
+			</span>
+		)
+	},
+	{
+		title:
+			"Queen's Gazette - Ontario's 2025 high school financial literacy requirement",
+		image: resource7.src,
+		link: 'https://www.queensu.ca/gazette/stories/back-school-wish-list-ontario-s-2025-high-school-financial-literacy-requirement',
+		description: (
+			<span className='text-sm'>
+				Six ways to meaningfully and effectively integrate financial literacy
+				into high school learning without high-stakes testing.
+			</span>
+		)
+	},
+	{
+		title: 'Ontario Newsroom - A Stronger OSSD',
+		image: resource8.src,
+		link: 'https://news.ontario.ca/en/backgrounder/1004649/modern-relevant-and-skills-focused-a-stronger-ontario-high-school-diploma',
+		description: (
+			<span className='text-sm'>
+				A new financial literacy graduation requirement, elevating math
+				education for students and staff, etc.
+			</span>
+		)
+	},
+	{
+		title: 'Statista - Consumer Price Index in Canada from 2000 to 2023',
+		image: resource9.src,
+		link: 'https://news.ontario.ca/en/backgrounder/1004649/modern-relevant-and-skills-focused-a-stronger-ontario-high-school-diploma',
+		description: (
+			<span className='text-sm'>
+				Adapted from Statistics Canada, statcan.gc.ca, 2000 to 2022.
+			</span>
+		)
 	}
 ];
